d7ac89eae3ce45f6dbfd4f9fd67fa1b280e4ae8a
Leo-Brd <leobordet.pro@gmail.com>
2024-06-16 15:26:17 +0200

    extract tilemap function

--- game.py
+++ game.py
@@ -58,6 +58,8 @@ class Game:
         
         self.games_objects = [self.player]
 
+        print(self.tilemap.extract([('large_decor', 2)], keep=True))
+
         self.scroll = [0, 0]
 
     def run(self):
